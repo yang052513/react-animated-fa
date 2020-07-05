29157957eb7d030cbf60186e690423b97d521d17
yang052513 <liyang0525.ly@gmail.com>
2020-07-04 23:32:35 -0700

    component layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
-console.log('hello')
+import React from 'react'
+
+interface Props {
+  iconUrl: string
+  link: string
+}
+
+export const Icon: React.FC<Props> = ({ iconUrl, link }) => {
+  return (
+    <a className="social-media-icon" href={link}>
+      <i className={iconUrl}></i>
+    </a>
+  )
+}
